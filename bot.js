2a18cfc8c23b2b7f6884ab17abeb109c7fc7e659
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-17 22:16:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ if(message.content.startsWith(prefix + "youtube")) {
 		try {
 			get('https://aws.random.cat/meow').then(res => {
 				const embed = new Discord.RichEmbed()
-				.setTitle(":meow: Un chat sauvage apparaît")
+				.setTitle(":smiley_cat: Un chat sauvage apparaît")
 				.setImage(res.body.file)
 				.setColor("0x88CC14")
 				return message.channel.send({embed});
