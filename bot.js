95b60149a7b53c7b6fdd781beda1c5c8048569fa
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-17 22:47:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,25 +132,7 @@ if(message.content.startsWith(prefix + "youtube")) {
           client.channels.findAll('name', 'u-chat').map(channel => channel.send({embed}))
           message.delete();
 }
-	
-	if(message.content.startsWith(prefix + "mcachievement")) {
- let [title, contents] = args.join(" ").split("|");
-  if(!contents) {
-    [title, contents] = ["Achievement Get!", title];
-  }
-  let rnd = Math.floor((Math.random() * 39) + 1);
-  if(args.join(" ").toLowerCase().includes("burn")) rnd = 38;
-  if(args.join(" ").toLowerCase().includes("cookie")) rnd = 21;
-  if(args.join(" ").toLowerCase().includes("cake")) rnd = 10;
-  if(args.join(" ").toLowerCase().includes("diamond")) rnd = 29;
-
-  if(title.length > 10 || contents.length > 10) return message.edit("Max Length: 10 Characters. Soz.").then(message.delete.bind(message), 2000);
-  const url = `https://www.minecraftskinstealer.com/achievement/a.php?i=${rnd}&h=${encodeURIComponent(title)}&t=${encodeURIComponent(contents)}`;
-  snekfetch.get(url)
-   .then(r=>message.channel.send("", {files:[{attachment: r.body}]}));
-  message.delete();
-}
-	
+       
 		if(message.content.startsWith(prefix + 'meow')) {
 		try {
 			get('https://aws.random.cat/meow').then(res => {
