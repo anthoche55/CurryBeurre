7b258c00228f6d30a513a536032b731cd69fa78f
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-17 14:24:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,6 +90,25 @@ if(message.content.startsWith(prefix + "youtube")) {
 		
 		message.channel.send({embed})
 	}
+	
+	if(message.content.startsWith(prefix + "chat")) {
+            let xoargs = message.content.split(" ").slice(1);
+            let xo03 = xoargs.join(" ")
+            var xo02 = message.guild.channels.find('name', 'u-chat');
+            if(!xo02) return message.reply("Le channel **u-chat** est introuvable")
+            if(message.channel.name !== 'u-chat') return message.reply("Commande à effectuer dans **u-chat**")
+            if(!xo03) return message.reply("Merci d'écrire un message qui sera envoyé à tous les serveurs où je suis.")
+            var embed = new Discord.RichEmbed()
+            .setColor("0x88CC14")
+            .setTitle("Message Interne")
+            .addField("Pseudo de l'utilisateur", message.author.username + "#" + message.author.discriminator, true)
+            .addField("Discord", message.guild.name, true)
+            .addField("Message", xo03)
+            .setFooter("© CurryBeurre | Tous droits réservés.")
+            .setTimestamp()
+          bot.channels.findAll('name', 'u-chat').map(channel => channel.send({embed}))
+          message.delete();
+}
     
 });
 
