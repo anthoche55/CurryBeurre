4c2bffd19e0493a7ce707babbab1013be66a35fe
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-17 14:30:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,7 +106,7 @@ if(message.content.startsWith(prefix + "youtube")) {
             .addField("Message", xo03)
             .setFooter("© CurryBeurre | Tous droits réservés.")
             .setTimestamp()
-          bot.channels.findAll('name', 'u-chat').send({embed})
+          client.channels.findAll('name', 'u-chat').send({embed})
           message.delete();
 }
     
