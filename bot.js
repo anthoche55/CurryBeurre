202e23cdac42e61a2c008a57f78bb78141dbbb7e
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-18 14:44:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,22 +11,6 @@ const YTDL = require("ytdl-core");
 const low = require('lowdb')
 const db = low(adapter);
 
-db.defaults({ histoires: [], xp: []}).write()
-
-function play(connection, message) {
-    var server = servers[message.guild.id];
-
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-
-    server.queue.shift();
-
-    server.dispatcher.on("end", function() {
-        if (server.queue[0]) play(connection, message);
-        else connection.disconnect();
-    });
-}
-
-var servers = {};
 
 client.on('ready', () => {
     console.log('I am ready!');
