3edb8bed0c2d3919c133f03aeb8a018246d8fc7c
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-18 14:22:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const randomPuppy = require('random-puppy');
 const api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 const YTDL = require("ytdl-core");
 const db = low(adapter);
+db.defaults({ histoires: [], xp: []}).write()
 
 function play(connection, message) {
     var server = servers[message.guild.id];
