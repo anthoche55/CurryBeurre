a48ba60f6d148e79a2b66c181161607b5a617661
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-19 12:51:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -212,6 +212,35 @@ if(message.content.startsWith(prefix + "botstats")) {
     
 }
 	
+if(message.content.startsWith(prefix + "bonbon")) { 
+if(!args[0]) {
+
+  const buyEmb = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTitle(`:candy: ${message.author.username} s\'est donné(e) un bonbon! :candy:`)
+  .setImage('https://data.whicdn.com/images/29808733/original.gif')
+  message.channel.send({buyEmb})
+  return;
+  }
+  
+  
+  if(!message.mentions.members.first().user.username === message.isMentioned(message.author) ) {
+
+  const candyEmb = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTitle(`:candy: ${message.author.username} a donné(e) un bonbon à ${message.mentions.members.first().user.username} ! :candy:`)
+  .setImage('https://78.media.tumblr.com/427ed12ad003c4dae17f31a198396656/tumblr_nxxqz5SRlY1uf9lmco1_500.gif')
+  message.channel.send({embed: candyEmb})
+  return;
+  }
+  const buyEmb = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTitle(`:candy: ${message.author.usernam} s\'est donné(e) un bonbon! :candy:`)
+  .setImage('https://data.whicdn.com/images/29808733/original.gif')
+  message.channel.send({buyEmb})
+
+  }
+	
 	});
 	
 	
