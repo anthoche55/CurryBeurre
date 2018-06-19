9950b106cffa9bde87021a9f46119538c9ed3360
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-19 12:42:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,21 +16,6 @@ client.on('ready', () => {
   });
 });
 
-function play(connection, message) {
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {
-        filter: "audioonly"
-    }));
-
-    server.queue.shift();
-
-    server.dispatcher.on("end", function() {
-        if (server.queue[0]) play(connection, message);
-        else connection.disconnect();
-    });
-
-}
-var servers = {};
 
 client.on('message', message => {
     
