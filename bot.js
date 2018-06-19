1da37b3fd5d970c8223443ee11c0c8a22aad2d10
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-06-19 18:36:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
     client.user.setStatus('idle')
     client.user.setPresence({
       game: {
-        name: 'cb!help | By Cecemel_PvP',
+        name: 'cb!help | By Cecemel_PvP | ${client.guilds.size} serveurs',
         type: 1
       }
   });
