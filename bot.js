538191755481bc29b7ee73ea1ec3ccb0e494c950
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-16 21:22:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,11 +21,17 @@ client.on('ready', () => {
 client.on('message', message => {
 	
 if(message.content.startsWith("prob")) {
-let erreure = new Discord.RichEmbed()
-	.setTitle("Petit souci")
-	.addField("Erreure", "Bonjour, la commande \"cb!chat\" est désactivée pour un moment car il y a un petit problème\nLa commande va revenir dans un petit bout de temps")
-	.setFooter("Cordialement, Cecemel_PvP, et encore toutes mes excuses")
-client.channels.findAll('name', 'u-chat').map(channel => channel.send({erreure}))
+
+client.channels.findAll('name', 'u-chat').map(channel => channel.send({embed: {
+    title: "Petit souci",
+    fields: [{
+        name: "Erreure",
+        value: "Erreure", "Bonjour, la commande \"cb!chat\" est désactivée pour un moment car il y a un petit problème\nLa commande va revenir dans un petit bout de temps"
+      }
+    ]
+}
+});
+)
 }
 
 var answers = [
