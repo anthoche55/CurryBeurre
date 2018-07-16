842f5301f1ae836d9ec326046a3a54adbfb8bd3a
BuildTools <cedricthauvoye1@gmail.com>
2018-07-16 18:42:07 +0200

    Invite-msg

--- bot.js
+++ bot.js
@@ -47,6 +47,14 @@ if(message.content.startsWith(prefix + "invite")) {
 message.channel.send({embed});
 }
 	
+if(message.content.startsWith(prefix + "invite-msg")) {
+	const embed = new Discord.RichEmbed()
+	.setTitle("Message à envoyer pour une pub pour le bot")
+	.setColor(0x00AE86)
+	.addField("Message", "Bonjour,\nVous voulez un bot fun;\n	modération;\n	administration;\n	avec un salon pour discuter avec tout les gens des serveurs qui ont le bot ? \n Le bot Curry Beurre est pour vous,\nLe serveur de support : https://discord.gg/2PQ4rfT \nVoici également le lien pour inviter le bot dans votre serveur : https://discordapp.com/api/oauth2/authorize?client_id=457540475851767808&permissions=8&scope=bot")
+	message.channel.send(embed)
+}
+
 
 if(message.content.startsWith(prefix + "blague")) {
 	const embed = new Discord.RichEmbed()
