dc6cf759af4b230ee7766571390eb52c6bf3080d
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-20 18:49:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -442,6 +442,42 @@ if(message.content.startsWith(prefix + "report")) {
 
 }
 	
+if(message.content.startsWith(prefix + "mute")) {
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute) return message.reply("Je ne peux pas trouver l'utilisateur");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Je ne peux pas mute cet utilisateur");
+  let muterole = message.guild.roles.find(`name`, "muté");
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "muté",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("Tu n\'as pas spécifié un temps");
+
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> a été mute pour ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> a été unmute!`);
+  }, ms(mutetime));
+
+
+}
+	
 });	
 	
 client.on("guildCreate", guild => {
