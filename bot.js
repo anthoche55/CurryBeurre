d76515e706d0fea27c74f838b99456c82760904a
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-09-05 17:46:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,15 +30,6 @@ if(message.author.equals(bot.user)) return;
                 break;
             }else{
                 if(message.channel.name === "fastpub"){
-                    if(message.author.username==="MEE6"){
-                        var embed = new Discord.RichEmbed()
-                            .setColor(0xf46842)
-                            .setAuthor("Envoyer une pub")
-                            .setDescription(":no_entry_sign: Vous ne pouvez pas utiliser Mee6 pour faire votre pub")
-                            .setFooter(bot.user.username+" - by Cecemel_PvP#9876 | Ft. Kalyax#4031")
-                        message.channel.sendEmbed(embed);
-                    return;
-                    }
                     if(message.author.id==="274636300332695562"||message.author.id==="347043099010269184"||message.author.id==="317221808405348364"||message.author.id==="394787892351991808"||message.author.id==="296357024864665605"||message.author.id==="398096736880427018"){
                         var embed = new Discord.RichEmbed()
                             .setColor(0xf46842)
