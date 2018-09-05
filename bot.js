33b3a54290bbe7ff24a8fa17df7b9bb3f422673d
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-09-05 16:50:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js');
-const prefix = ".";
+const prefix = "!";
 const client = new Discord.Client();
 const bot = new Discord.Client();
 
 client.on("message", async message => {
   
 });
+
+bot.on("ready", () => {
+    bot.user.setActivity(prefix+"help | "+bot.guilds.size+" serveurs | "+bot.users.size+" utilisateurs");
+    console.log(bot.user.username+" ON - by Kalyax#4031");
+})
 
 client.login(process.env.BOT_TOKEN);
